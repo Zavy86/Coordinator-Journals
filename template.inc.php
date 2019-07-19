0bad5122e38a19837f55c41a85d12cd34369a64b
Manuel Zavatta <manuel.zavatta@gmail.com>
2019-07-19 13:34:36 +0200

    Template

---
+++ template.inc.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Journals - Template
+ *
+ * @package Coordinator\Modules\Journals
+ * @author  Manuel Zavatta <manuel.zavatta@gmail.com>
+ * @link    http://www.coordinator.it
+ */
+ // build application
+ $app=new strApplication();
+ // build nav object
+ $nav=new strNav("nav-tabs");
+ // dashboard
+ $nav->addItem(api_icon("fa-th-large",null,"hidden-link"),api_url(["scr"=>"dashboard"]));
+ // tags
+ if(explode("_",SCRIPT)[0]=="tags"){
+  $nav->addItem(api_text("nav-tags-list"),api_url(["scr"=>"tags_list"]));
+  // operations
+  if($tag_obj->id && in_array(SCRIPT,array("tags_view","tags_edit"))){
+   $nav->addItem(api_text("nav-operations"),null,null,"active");
+   $nav->addSubItem(api_text("nav-tags-operations-edit"),api_url(["scr"=>"tags_edit","idTag"=>$tag_obj->id]),(api_checkAuthorization("journals-manage")));
+  }else{
+   $nav->addItem(api_text("nav-tags-add"),api_url(["scr"=>"tags_edit"]),(api_checkAuthorization("journals-manage")));
+  }
+ }
+ // add nav to html
+ $app->addContent($nav->render(false));
+?>
