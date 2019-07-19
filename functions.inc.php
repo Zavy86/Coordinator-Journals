2aea722a1d525957809810d7771adc7b04f2c712
Manuel Zavatta <manuel.zavatta@gmail.com>
2019-07-19 13:34:23 +0200

    Functions

---
+++ functions.inc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Journals Functions
+ *
+ * @package Coordinator\Modules\Journals
+ * @author  Manuel Zavatta <manuel.zavatta@gmail.com>
+ * @link    http://www.coordinator.it
+ */
+
+ // include classes
+ require_once(DIR."modules/journals/classes/cJournalsTag.class.php");
+
+?>
