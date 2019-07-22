4035cd72ca1eca9f90a8d1851c6e7381ede81591
Manuel Zavatta <manuel.zavatta@gmail.com>
2019-07-22 14:20:42 +0200

    Template updated

--- template.inc.php
+++ template.inc.php
@@ -12,17 +12,8 @@
  $nav=new strNav("nav-tabs");
  // dashboard
  $nav->addItem(api_icon("fa-th-large",null,"hidden-link"),api_url(["scr"=>"dashboard"]));
- // tags
- if(explode("_",SCRIPT)[0]=="tags"){
-  $nav->addItem(api_text("nav-tags-list"),api_url(["scr"=>"tags_list"]));
-  // operations
-  if($tag_obj->id && in_array(SCRIPT,["tags_view","tags_edit"])){
-   $nav->addItem(api_text("nav-operations"),null,null,"active");
-   $nav->addSubItem(api_text("nav-tags-operations-edit"),api_url(["scr"=>"tags_edit","idTag"=>$tag_obj->id]),(api_checkAuthorization("journals-manage")));
-  }else{
-   $nav->addItem(api_text("nav-tags-add"),api_url(["scr"=>"tags_edit"]),(api_checkAuthorization("journals-manage")));
-  }
- }
+ $nav->addItem(api_text("nav-settings"));
+ $nav->addSubItem(api_text("nav-settings-tags"),api_url(["scr"=>"tags_list"]));
  // add nav to html
  $app->addContent($nav->render(false));
 ?>
