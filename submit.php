63702a1a41d3d9c5a834f2683c5b8ed12df60b9f
Manuel Zavatta <manuel.zavatta@gmail.com>
2019-07-22 14:21:57 +0200

    Submit calls

--- submit.php
+++ submit.php
@@ -16,7 +16,13 @@
   case "tag_delete":tag("delete");break;
   case "tag_undelete":tag("undelete");break;
   case "tag_remove":tag("remove");break;
-
+  // tasks
+  case "task_store":task("store");break;
+  case "task_complete":task("complete");break;
+  case "task_uncomplete":task("uncomplete");break;
+  case "task_remove":task("remove");break;
+  // notebooks
+  case "notebook_store":notebook("store");break;
   // default
   default:
    api_alerts_add(api_text("alert_submitFunctionNotFound",[MODULE,SCRIPT,ACTION]),"danger");
@@ -30,7 +36,7 @@
   */
  function tag($action){
   // check authorizations
-  api_checkAuthorization("journals-manage","dashboard");
+  //api_checkAuthorization("journals-manage","dashboard");
   // get object
   $tag_obj=new cJournalsTag($_REQUEST['idTag']);
   api_dump($tag_obj,"tag object");
@@ -63,6 +69,81 @@
   }catch(Exception $e){
    // dump, alert and redirect
    api_redirect_exception($e,api_url(["scr"=>"tags_list","idTag"=>$tag_obj->id]),"cJournalsTag-alert-error");
+  }
+ }
+
+ /**
+  * Task
+  *
+  * @param string $action Object action
+  */
+ function task($action){
+  // check authorizations
+  //api_checkAuthorization("journals-manage","dashboard");
+  // get object
+  $task_obj=new cJournalsTask($_REQUEST['idTask']);
+  api_dump($task_obj,"task object");
+  // check object
+  if($action!="store" && !$task_obj->id){api_alerts_add(api_text("cJournalsTask-alert-exists"),"danger");api_redirect("?mod=".MODULE."&scr=tasks_list");}
+  // execution
+  try{
+   switch($action){
+    case "store":
+     $task_obj->store($_REQUEST);
+     $task_obj->store_tags($_REQUEST['tags']);
+     api_alerts_add(api_text("cJournalsTask-alert-stored"),"success");
+     break;
+    case "complete":
+     $task_obj->complete();
+     api_alerts_add(api_text("cJournalsTask-alert-completed"),"success");
+     break;
+    case "uncomplete":
+     $task_obj->uncomplete();
+     api_alerts_add(api_text("cJournalsTask-alert-uncompleted"),"warning");
+     break;
+    case "remove":
+     $task_obj->remove();
+     api_alerts_add(api_text("cJournalsTask-alert-removed"),"warning");
+     break;
+    default:
+     throw new Exception("Task action \"".$action."\" was not defined..");
+   }
+   // redirect
+   api_redirect(api_return_url(["scr"=>"tasks_list","idTask"=>$task_obj->id]));
+  }catch(Exception $e){
+   // dump, alert and redirect
+   api_redirect_exception($e,api_url(["scr"=>"tasks_list","idTask"=>$task_obj->id]),"cJournalsTask-alert-error");
+  }
+ }
+
+ /**
+  * Notebook
+  *
+  * @param string $action Object action
+  */
+ function notebook($action){
+  // check authorizations
+  //api_checkAuthorization("journals-manage","dashboard");
+  // get object
+  $notebook_obj=new cJournalsNotebook($_REQUEST['idNotebook']);
+  api_dump($notebook_obj,"notebook object");
+  // check object
+  if($action!="store" && !$notebook_obj->id){api_alerts_add(api_text("cJournalsNotebook-alert-exists"),"danger");api_redirect("?mod=".MODULE."&scr=notebooks_list");}
+  // execution
+  try{
+   switch($action){
+    case "store":
+     $notebook_obj->store($_REQUEST);
+     api_alerts_add(api_text("cJournalsNotebook-alert-stored"),"success");
+     break;
+    default:
+     throw new Exception("Notebook action \"".$action."\" was not defined..");
+   }
+   // redirect
+   api_redirect(api_return_url(["scr"=>"notebooks_list","idNotebook"=>$notebook_obj->id]));
+  }catch(Exception $e){
+   // dump, alert and redirect
+   api_redirect_exception($e,api_url(["scr"=>"notebooks_list","idNotebook"=>$notebook_obj->id]),"cJournalsNotebook-alert-error");
   }
  }
 
