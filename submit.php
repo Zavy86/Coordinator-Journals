1f548ce999ecaf0b94f1a3500c2d7eda5c81fc3f
Manuel Zavatta <manuel.zavatta@gmail.com>
2019-07-22 14:44:32 +0200

    Authorizations check

--- submit.php
+++ submit.php
@@ -36,12 +36,14 @@
   */
  function tag($action){
   // check authorizations
-  //api_checkAuthorization("journals-manage","dashboard");
+  api_checkAuthorization("journals-usage","dashboard");
   // get object
   $tag_obj=new cJournalsTag($_REQUEST['idTag']);
   api_dump($tag_obj,"tag object");
   // check object
   if($action!="store" && !$tag_obj->id){api_alerts_add(api_text("cJournalsTag-alert-exists"),"danger");api_redirect("?mod=".MODULE."&scr=tags_list");}
+  // check owner
+  if($tag_obj->id && $tag_obj->fkUser!=$GLOBALS['session']->user->id && !api_checkAuthorization("journals-manage")){api_alerts_add(api_text("cJournalsTag-alert-unauthorized"),"danger");api_redirect(api_url(["scr"=>"dashboard"]));}
   // execution
   try{
    switch($action){
@@ -79,12 +81,14 @@
   */
  function task($action){
   // check authorizations
-  //api_checkAuthorization("journals-manage","dashboard");
+  api_checkAuthorization("journals-usage","dashboard");
   // get object
   $task_obj=new cJournalsTask($_REQUEST['idTask']);
   api_dump($task_obj,"task object");
   // check object
   if($action!="store" && !$task_obj->id){api_alerts_add(api_text("cJournalsTask-alert-exists"),"danger");api_redirect("?mod=".MODULE."&scr=tasks_list");}
+  // check owner
+  if($task_obj->id && $task_obj->fkUser!=$GLOBALS['session']->user->id && !api_checkAuthorization("journals-manage")){api_alerts_add(api_text("cJournalsTask-alert-unauthorized"),"danger");api_redirect(api_url(["scr"=>"dashboard"]));}
   // execution
   try{
    switch($action){
@@ -123,12 +127,14 @@
   */
  function notebook($action){
   // check authorizations
-  //api_checkAuthorization("journals-manage","dashboard");
+  api_checkAuthorization("journals-usage","dashboard");
   // get object
   $notebook_obj=new cJournalsNotebook($_REQUEST['idNotebook']);
   api_dump($notebook_obj,"notebook object");
   // check object
   if($action!="store" && !$notebook_obj->id){api_alerts_add(api_text("cJournalsNotebook-alert-exists"),"danger");api_redirect("?mod=".MODULE."&scr=notebooks_list");}
+  // check owner
+  if($notebook_obj->id && $notebook_obj->fkUser!=$GLOBALS['session']->user->id && !api_checkAuthorization("journals-manage")){api_alerts_add(api_text("cJournalsNotebook-alert-unauthorized"),"danger");api_redirect(api_url(["scr"=>"dashboard"]));}
   // execution
   try{
    switch($action){
